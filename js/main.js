36790851d510faf39b737163e94376dde85c9469
Pyae Thein <pyaethein246@gmail.com>
2022-04-10 01:36:25 +0630

    commit on 'master'

--- js/main.js
+++ js/main.js
@@ -1,19 +1,7 @@
 const navMenu = document.getElementById('nav-menu');
 const navList = document.getElementById('nav-list');
-const navClose = document.getElementById('nav-close');
 
-
-  navMenu.addEventListener('click', () => {
-    navList.classList.add('active');
-    navMenu.classList.add('active');
-    navClose.classList.add('active');
-  })
-
-
-
-  navClose.addEventListener('click', () => {
-    navList.classList.remove('active');
-    navMenu.classList.remove('active');
-    navClose.classList.remove('active');
-  })
-
+navMenu.addEventListener('click', () => {
+  navList.classList.toggle('active');
+  navMenu.classList.toggle('fa-xmark');
+})
